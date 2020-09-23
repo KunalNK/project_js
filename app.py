42989871b342bd1b0bc6da079ddf843fbcd1456e
Kunal K <kkunal@localhost.localdomain>
2020-09-23 07:07:01 -0700

    json data

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import urllib.request, json
+import csv
 
 url = "https://gorest.co.in/public-api/users"
 response = urllib.request.urlopen(url)
 mydata = json.loads(response.read())
 print(type(mydata['data']))
-for person in mydata['data']:
-    names=(person['name'])
-    print(names)
+
+with open('myname.csv', 'w', newline='') as f:
+    thewriter = csv.writer(f)
+    thewriter.writerow(['Name','Emails'])
+    for person in mydata['data']:
+       names=(person['name'])
+       emails=(person['email'])
+       print(names)
+       thewriter.writerow([names,emails])
+    
     
    
